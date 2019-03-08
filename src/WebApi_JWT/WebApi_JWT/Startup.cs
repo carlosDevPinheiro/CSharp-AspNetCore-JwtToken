6998cb1e7c03cc2c7c07b10f9c3bc36ff56649d2
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2019-03-08 12:42:14 -0300

    implementado

---
+++ src/WebApi_JWT/WebApi_JWT/Startup.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi_JWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+     
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(option =>
+               {
+                   option.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+
+                       ValidIssuer = "Teste.Securiry.Bearer",
+                       ValidAudience = "Teste.Securiry.Bearer",
+                       IssuerSigningKey = ProviderJWT.JWTSecurityKey.Create("Secret_Key-12345678")
+                   };
+
+                   option.Events = new JwtBearerEvents
+                   {
+                       OnAuthenticationFailed = context =>
+                       {
+                           Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                           return Task.CompletedTask;
+                       },
+                       OnTokenValidated = context =>
+                       {
+                           Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                           return Task.CompletedTask;
+                       }
+                   };
+               });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("UsuarioAPI",
+                    policy => policy.RequireClaim("UsuarioAPINumero"));
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+      
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
